f16dda87b2a401337b3de2dfe2e19dbac396cdad
Kyron4251 <101371579+Kyron4251@users.noreply.github.com>
2023-08-20 19:32:48 -0400

    Profesionales beta

--- clinica_sarman/src/subpage/profesionales.js
+++ clinica_sarman/src/subpage/profesionales.js
@@ -2,14 +2,18 @@ import React from "react";
 import { Navigation_bar} from "../components/navbar";
 import { Banner} from "../components/banner";
 import { Footer} from "../components/footer";
-
+import {Card_pro_1} from "../components/card_pro_1";
 //Home page content
 function Doctors() {
     return (
       <React.Fragment>
+
       <Banner/>
       <Navigation_bar/>
+      <Card_pro_1/>
+
       <Footer/>
+
       </React.Fragment>
     );
   }
